089daca010cee148349d6d8075c4bd67142e08e2
Tianhang Liu <tianhanl@usc.edu>
2018-05-22 17:37:16 +0800

    add notice when article list is empty

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import ArticleListItem from './ArticleListItem';
 import { PaddingLessUl } from './ShardStyledComponents';
 
+const Notice = styled.p`
+  text-align: center;
+  color: #a8a8a8;
+`;
+
 const ArticleList = props => {
   const { articles } = props;
+  if (!articles || !articles.length) {
+    return <Notice>Well, empty article list</Notice>;
+  }
   return (
     <PaddingLessUl>
       {articles.map(article => (
