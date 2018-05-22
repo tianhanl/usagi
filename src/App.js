f77ebf9900ee3928d1cd1d07892b4113c982d9f5
Tianhang Liu <tianhanl@usc.edu>
2018-05-22 17:58:25 +0800

    add styles for app

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import { Provider } from 'react-redux';
 import configureStore from './configureStore';
 import Header from './components/Header';
 import ArticleListView from './views/ArticleListView';
+import styled from 'styled-components';
+import {
+  FlexColumnWrapper,
+  StickyMain
+} from './components/ShardStyledComponents';
 
 const store = configureStore();
+
+const Wrapper = FlexColumnWrapper.extend`
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #515151;
+  margin-top: 0;
+  min-height: 99vh;
+  box-sizing: border-box;
+`;
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
+        <Wrapper>
           <Header blogTitle={'Usagi'} />
-          <ArticleListView />
-        </div>
+          <StickyMain>
+            <ArticleListView />
+          </StickyMain>
+        </Wrapper>
       </Provider>
     );
   }
--- src/components/ShardStyledComponents.js
+++ src/components/ShardStyledComponents.js
@@ -3,3 +3,15 @@ import styled from 'styled-components';
 export const PaddingLessUl = styled.ul`
   padding: 0;
 `;
+
+export const FlexWrapper = styled.div`
+  display: flex;
+`;
+
+export const FlexColumnWrapper = FlexWrapper.extend`
+  flex-direction: column;
+`;
+
+export const StickyMain = styled.div`
+  flex: 1 0 80%;
+`;
