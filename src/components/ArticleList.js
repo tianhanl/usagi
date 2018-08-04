351407bb999a452527e4caca4b81a754ffa2233c
Tianhang Liu <tianhanl@usc.edu>
2018-08-04 17:49:18 +0800

    introduce react motion

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,24 +2,59 @@ import React from 'react';
 import styled from 'styled-components';
 import ArticleListItem from './ArticleListItem';
 import { PaddingLessUl } from './ShardStyledComponents';
-
+import { TransitionMotion, spring, presets } from 'react-motion';
 const Notice = styled.p`
   text-align: center;
   color: #a8a8a8;
 `;
 
-const ArticleList = props => {
-  const { articles } = props;
-  if (!articles || !articles.length) {
-    return <Notice>Well, empty article list</Notice>;
+class ArticleList extends React.Component {
+  getDefaultStyles = () => {
+    const { articles } = this.props;
+    return articles.map(article => ({
+      key: article.id,
+      style: {
+        height: 0,
+        opacity: 1
+      },
+      data: article
+    }));
+  };
+
+  getStyles = () => {
+    const { articles } = this.props;
+    return articles.map(article => ({
+      key: article.id,
+      style: {
+        height: spring(60, presets.gentle),
+        opacity: spring(1, presets.gentle)
+      },
+      data: article
+    }));
+  };
+
+  willEnter = () => ({
+    height: 0,
+    opacity: 1
+  });
+
+  render() {
+    const { articles } = this.props;
+    if (!articles || !articles.length) {
+      return <Notice>Well, empty article list</Notice>;
+    }
+    return (
+      <TransitionMotion styles={this.getStyles} willEnter={this.willEnter}>
+        {styles => (
+          <PaddingLessUl>
+            {styles.map(({ key, style, data }) => (
+              <ArticleListItem style={style} key={key} article={data} />
+            ))}
+          </PaddingLessUl>
+        )}
+      </TransitionMotion>
+    );
   }
-  return (
-    <PaddingLessUl>
-      {articles.map(article => (
-        <ArticleListItem key={article.id} article={article} />
-      ))}
-    </PaddingLessUl>
-  );
-};
+}
 
 export default ArticleList;
